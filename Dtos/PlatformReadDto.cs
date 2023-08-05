c89ccbde20a2ceaafad8b56aa8c917a76c2c3e0b
chiefnaheem <naheemademola21@gmail.com>
2023-08-04 21:41:08 -0700

    added another feature

--- Dtos/PlatformReadDto.cs
+++ Dtos/PlatformReadDto.cs
@@ -6,5 +6,6 @@ namespace PlatformService.Dtos
         public int Id { get; set; }
         public string Name { get; set; }
         public string Publisher { get; set; }
+        public string Cost { get; set; }
     }
 }
