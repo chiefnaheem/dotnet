cdd97639e7099e8504b5b022e63f5175a03d5deb
chiefnaheem <naheemademola21@gmail.com>
2023-08-04 20:53:35 -0700

    setting up dtos

---
+++ Dtos/PlatformReadDto.cs
@@ -0,0 +1,10 @@
+namespace PlatformService.Dtos
+
+{
+    public class PlatformRead
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+    }
+}
